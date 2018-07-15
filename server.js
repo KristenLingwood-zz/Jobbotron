977fb462b0e6cec06117c70d2d95fdd52fbfc8f3
KristenLingwood <kristen.lingwood@gmail.com>
2018-07-15 15:11:56 -0700

    updated env var for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./index');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('Server starting on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server starting on port ${PORT}`);
 });
