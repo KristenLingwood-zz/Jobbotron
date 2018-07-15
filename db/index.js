d0c3f8d20a30e689790ef996751aedbe3caa4fcf
KristenLingwood <kristen.lingwood@gmail.com>
2018-07-15 15:33:04 -0700

    update db.index for heroku

--- db/index.js
+++ db/index.js
@@ -2,7 +2,8 @@ const { Client } = require('pg');
 const dbName = process.env.NODE_ENV === 'test' ? 'jobbotron_test' : 'jobbotron';
 
 const client = new Client({
-  connectionString: `postgresql://localhost/${dbName}`
+  connectionString:
+    process.env.DATABASE_URL || `postgresql://localhost/${dbName}`
 });
 
 client.connect();
